8967ac203ebb8746ea5edb39f74fe03e1d0694d0
Manabu Matsuzaki <matsumana@gmail.com>
2015-09-22 23:01:53 +0900

    SQLを実行するロジックを追加

--- app/lib/Isucon/Web.pm
+++ app/lib/Isucon/Web.pm
@@ -9,17 +9,43 @@ use DBIx::Sunny;
 
 # TODO 後で消す
 use Data::Dumper;
+use DBIx::QueryLog;
 use Log::Minimal;
+
+sub dbh {
+    my ($self) = @_;
+    $self->{_dbh} ||= do {
+        my $host      = '127.0.0.1';
+        my $port      =  3306;
+        my $database = 'isucon';
+        my $user     = 'root';
+        my $pass     = 'root';
+        DBIx::Sunny->connect(
+            "dbi:mysql:database=$database;host=$host;port=$port", $user, $pass, {
+                RaiseError => 1,
+                PrintError => 0,
+                AutoInactiveDestroy => 1,
+                mysql_enable_utf8   => 1,
+                mysql_auto_reconnect => 1,
+            },
+        );
+    };
+}
 
 get '/' => [qw()] => sub {
     my ($self, $c) = @_;
 
+    my $count = $self->dbh->select_one(
+        'SELECT COUNT(*) FROM table0 WHERE col1 != ?',
+        999
+    );
+
 # TODO 後で消す
-debugf("%s %d", "hoge!!", 100);
+debugf("%s %d", "hoge!!", $count);
 
     $c->render('index.tx', {
         hoge => 'aaa',
-        fuga => 123,
+        fuga => $count,
         piyo => 'bbb',
     });
 };
